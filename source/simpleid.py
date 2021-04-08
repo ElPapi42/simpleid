bfdd0e6ed1b22759369e68a361b9fb65c4cee753
elpapi42 <whitman-2@hotmail.com>
2021-04-08 01:08:39 -0400

    Improved algorithm

--- source/simpleid.py
+++ source/simpleid.py
@@ -1,33 +1,41 @@
-from time import time
-from uuid import getnode
+from time import time, time_ns
+from os import getpid
 from typing import Union
 from threading import Lock
+from struct import pack
+from random import randint
 
 
 class SimpleId(object):
     """Value object that represent a 64bits integer id."""
 
-    _inc = 1
+    _pid = None
+    _inc = randint(0, 1000)
     _inc_lock = Lock()
+
+    __random = None
 
     def __init__(self, value:Union[str, int, None]=None):
         self._value:int = self._generate()
+    
+    @classmethod
+    def _random(cls):
+        """Random int once per process."""
+        pid = getpid()
+        if pid != cls._pid:
+            cls._pid = pid
+            cls.__random = randint(0, 1000)
+        return cls.__random
 
     def _generate(self):
         # Timestamp
-        output = int(time())
+        output = int(time() * 1000)
 
-        # Space for machine id
-        output *= 100000
+        # Space for process random id
+        output *= 1000
 
-        # Machine id
-        output += int(getnode() / 1000000000)
-
-        # Space for algorith version
-        output *= 10
-
-        # Algorithm version
-        output += 1
+        # Process random id
+        output += SimpleId._random()
 
         # Space for increment
         output *= 1000
---
+++ tests/test_simpleid.py
@@ -0,0 +1,30 @@
+import pytest
+from dataclasses import FrozenInstanceError
+
+from source import SimpleId
+
+
+def test_instance_simpleid():
+    sid = SimpleId()
+    print(sid)
+    assert True
+
+def test_collision_0():
+    qty = 1000000
+
+    # Generate list of ids
+    sids = [SimpleId().value for i in range(qty)]
+    print(sids[0], sids[-1])
+    sids_no_dupl = list(dict.fromkeys(sids))
+
+    assert len(sids) == len(sids_no_dupl)
+
+def test_simpleid_is_inmutable():
+    sid = SimpleId()
+
+    try:
+        sid.value = 0
+    except AttributeError:
+        return
+    
+    assert False
