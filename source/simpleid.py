4d91e0d8fd0a83c3a5d4113ab82ae141ee2627aa
elpapi42 <whitman-2@hotmail.com>
2021-04-11 00:29:09 -0400

    Improve get_logical_node clas function

--- source/simpleid.py
+++ source/simpleid.py
@@ -1,9 +1,11 @@
 from time import time, time_ns
-from os import getpid
+from os import getpid, urandom
+from secrets import randbits
+from uuid import getnode
 from typing import Union
 from threading import Lock
 from struct import pack
-from random import randint
+from random import Random, randint
 
 
 class SimpleId(object):
@@ -13,30 +15,45 @@ class SimpleId(object):
     _inc = randint(0, 100000)
     _inc_lock = Lock()
 
-    __random = None
+    __logical_node_name:str = ''
+    __logical_node_id:int
 
     def __init__(self, value:Union[str, int, None]=None):
-        self._value:int = self._generate()
+        self._value:int = self.generate()
     
     @classmethod
-    def _random(cls):
-        """Random int once per process."""
-        pid = getpid()
-        if pid != cls._pid:
-            cls._pid = pid
-            cls.__random = randint(0, 1000)
-        return cls.__random
+    def get_logical_node(cls):
+        """
+        Random id for machine-process pairs.
 
-    def _generate(self):
+        A machine-process pair is
+        also know as logical node.
+        """
+        # Get string representation of logical node
+        logical_node_name = f'{getnode()}:{getpid()}'
+
+        # New logical node id if its name was not set
+        if logical_node_name != cls.__logical_node_name:
+            cls.__logical_node_name = logical_node_name
+
+            # New random logical node id using the name as seed
+            rand = Random(x=logical_node_name)
+            cls.__logical_node_id = rand.randint(0, 1000)
+
+        return cls.__logical_node_id
+
+    def generate(self):
         timestamp = int(time() * 10)
 
-        process = SimpleId._random()
+        process = SimpleId.get_logical_node()
 
         with SimpleId._inc_lock:
             increment = SimpleId._inc
 
             # Increment counter
             SimpleId._inc = (SimpleId._inc + 1) % 100000
+        
+        #print(timestamp, process, increment)
 
         return (timestamp * 100000000) + (process * 100000) + (increment)
     
