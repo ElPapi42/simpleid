0f67701e1a1609098843bc896705f39192b0eef0
elpapi42 <whitman-2@hotmail.com>
2021-04-11 00:59:11 -0400

    Add tests for distributed environment

--- tests/test_simpleid.py
+++ tests/test_simpleid.py
@@ -1,30 +1,54 @@
 import pytest
-from dataclasses import FrozenInstanceError
+import itertools
+from multiprocessing import Pool
 
 from source import SimpleId
 
 
-def test_instance_simpleid():
-    sid = SimpleId()
-    print(sid)
-    assert True
+def sids_factory(n):
+    """
+    Generate n SimpleIds.
+
+    Returns a list of string representations
+    of the generated sids for convinience
+    """
+    return [str(SimpleId()) for _ in range(n)]
 
 def test_collision_0():
-    qty = 1000000
+    """
+    This test ensures sids work with
+    100% reliability on a single logical
+    node environment.
+    """
+    qty = 100000
 
     # Generate list of ids
-    sids = [SimpleId().value for i in range(qty)]
+    sids = sids_factory(qty)
 
     sids_no_dupl = list(dict.fromkeys(sids))
 
     assert len(sids) == len(sids_no_dupl)
 
-def test_simpleid_is_inmutable():
-    sid = SimpleId()
+def test_collision_multi_0():
+    """
+    This test ensures sids work with 
+    100% reliability on a distirbuted 
+    environment of multiple logical nodes.
+    """
+    logical_nodes = 100
+    sids_per_node = 1000
 
-    try:
-        sid.value = 0
-    except AttributeError:
-        return
+    # Iterable with the amount of sids to create per node
+    args = [sids_per_node for _ in range(logical_nodes)]
+
+    # Generate ids distributed in multiple logical nodes
+    with Pool(logical_nodes) as pool:
+        results = pool.map(sids_factory, args)
     
-    assert False
+    # Flatten output resulting in list of sids generated
+    sids = list(itertools.chain.from_iterable(results))
+
+    # Get unique sids
+    sids_no_dupl = list(dict.fromkeys(sids))
+
+    assert len(sids) == len(sids_no_dupl)
